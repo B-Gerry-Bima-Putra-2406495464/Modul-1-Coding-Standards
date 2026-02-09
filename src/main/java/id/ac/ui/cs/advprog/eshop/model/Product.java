e52e03b824d4bc442bfec6bfe5f63604f357132f
Gerrybp12 <gerry.bimaputra@gmail.com>
2026-02-09 23:07:10 +0700

    add product quantity tidak boleh negatif

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+import jakarta.validation.constraints.Min;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,5 +8,7 @@ import lombok.Setter;
 public class Product {
     private String productId;
     private String productName;
+
+    @Min(value = 0, message = "Quantity tidak boleh negatif")
     private int productQuantity;
 }
